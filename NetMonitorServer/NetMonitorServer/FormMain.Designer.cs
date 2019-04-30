3ee0f2a6eac1136f2d57176a70d76d59780737ee
ilia-98 <46720107+ilia-98@users.noreply.github.com>
2019-04-30 18:42:52 +0500

    Add files via upload

---
+++ NetMonitorServer/NetMonitorServer/FormMain.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace NetMonitorServer
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.ServerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.RestartToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelServerStatus = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.labelHardwareInfo = new System.Windows.Forms.Label();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.labelSelectedItem = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(12, 96);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(386, 359);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.SmallIcon;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ServerToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1090, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // ServerToolStripMenuItem
+            // 
+            this.ServerToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.RestartToolStripMenuItem});
+            this.ServerToolStripMenuItem.Name = "серверToolStripMenuItem";
+            this.ServerToolStripMenuItem.Size = new System.Drawing.Size(59, 20);
+            this.ServerToolStripMenuItem.Text = "Сервер";
+            // 
+            // RestartToolStripMenuItem
+            // 
+            this.RestartToolStripMenuItem.Name = "RestartToolStripMenuItem";
+            this.RestartToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.RestartToolStripMenuItem.Text = "Перезапуск";
+            this.RestartToolStripMenuItem.Click += new System.EventHandler(this.RestartToolStripMenuItem_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Статус сервера:";
+            // 
+            // labelServerStatus
+            // 
+            this.labelServerStatus.AutoSize = true;
+            this.labelServerStatus.ForeColor = System.Drawing.Color.Red;
+            this.labelServerStatus.Location = new System.Drawing.Point(104, 35);
+            this.labelServerStatus.Name = "labelServerStatus";
+            this.labelServerStatus.Size = new System.Drawing.Size(45, 13);
+            this.labelServerStatus.TabIndex = 6;
+            this.labelServerStatus.Text = "OFLINE";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label1.Location = new System.Drawing.Point(6, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 20);
+            this.label1.TabIndex = 7;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(672, 396);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(404, 27);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(686, 428);
+            this.tabControl1.TabIndex = 11;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.pictureBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(678, 402);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Экран";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.labelHardwareInfo);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(678, 402);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Информация";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // labelHardwareInfo
+            // 
+            this.labelHardwareInfo.AutoSize = true;
+            this.labelHardwareInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.labelHardwareInfo.Location = new System.Drawing.Point(7, 7);
+            this.labelHardwareInfo.Name = "labelHardwareInfo";
+            this.labelHardwareInfo.Size = new System.Drawing.Size(0, 20);
+            this.labelHardwareInfo.TabIndex = 0;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.label1);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(678, 402);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Нагрузка";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1300;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // labelSelectedItem
+            // 
+            this.labelSelectedItem.AutoSize = true;
+            this.labelSelectedItem.Location = new System.Drawing.Point(9, 65);
+            this.labelSelectedItem.Name = "labelSelectedItem";
+            this.labelSelectedItem.Size = new System.Drawing.Size(49, 13);
+            this.labelSelectedItem.TabIndex = 12;
+            this.labelSelectedItem.Text = "Выбран:";
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1090, 464);
+            this.Controls.Add(this.labelSelectedItem);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.labelServerStatus);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = global::NetMonitorServer.Properties.Resources.Main;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FormMain";
+            this.Text = "NetMonitor Server";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormMain_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        public System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem ServerToolStripMenuItem;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelServerStatus;
+        private System.Windows.Forms.ToolStripMenuItem RestartToolStripMenuItem;
+        public System.Windows.Forms.Label label1;
+        public System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label labelSelectedItem;
+        public System.Windows.Forms.Label labelHardwareInfo;
+    }
+}
+
---
+++ NetMonitorServer/NetMonitorServer/FormMain.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+namespace NetMonitorServer
+{
+    public partial class FormMain : Form
+    {
+        public WebSocketServer socketServer = new WebSocketServer(IPAddress.Any, 1348);
+
+        public List<Client> AllClients = new List<Client>();
+
+        Client _selectedClient = null;
+
+        public Client SelectedClient
+        {
+            get { return _selectedClient; }
+            set
+            {
+                if (value == null)
+                {
+                    labelSelectedItem.Text = "Выбран: " + "none";
+                }
+                else
+                {
+                    labelHardwareInfo.Text = "";
+
+                    if (value.HardwareInfo != null)
+                    foreach (var item in value.HardwareInfo)
+                            labelHardwareInfo.Text += item.Key + ": " + item.Value + "\n";
+
+                    labelSelectedItem.Text = "Выбран: " + value.MAC;
+                }
+
+                _selectedClient = value;
+            }
+        }
+
+        public List<Client> GetAllClietnsFromList()
+        {
+            List<Client> result = new List<Client>();
+            foreach (var item in listView1.Items)
+                result.Add((Client)item);
+            return result;
+        }
+
+        public void ServerStop()
+        {
+            socketServer.Stop();
+            Console.WriteLine("Сервер Выключен.");
+            labelServerStatus.Text = "OFLINE";
+            labelServerStatus.ForeColor = Color.Red;
+            timer1.Stop();
+        }
+
+        public void ServerStart()
+        {
+            socketServer.AddWebSocketService("/NetMonitorSocketService", () => new NetMonitorClient(this));
+            socketServer.Start();
+            Console.WriteLine("Сервер запущен. Ожидание подключений...");
+            labelServerStatus.Text = "ONLINE";
+            labelServerStatus.ForeColor = Color.Green;
+            timer1.Start();
+        }
+
+        public void ServerRestart()
+        {
+            ServerStop();
+            ServerStart();
+        }
+
+        public FormMain()
+        {
+            InitializeComponent();
+            ImageList imageList = new ImageList();
+            imageList.Images.Add(Properties.Resources.Circle_Red);
+            imageList.Images.Add(Properties.Resources.Circle_Green);
+            listView1.SmallImageList = imageList;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ServerStart();
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count != 1)
+            {
+                SelectedClient = null;
+                return;
+            }
+            Client selectClient = (Client)listView1.SelectedItems[0];
+            SelectedClient = selectClient;
+        }
+
+        private void RestartToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ServerRestart();
+        }
+
+        private void FormMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            ServerStop();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (SelectedClient != null)
+            {
+                if (SelectedClient.Available)
+                {
+                    SelectedClient.Send("Monitor_Info");
+                    SelectedClient.Send("Screenshot");
+                }
+            }
+        }
+    }
+}
