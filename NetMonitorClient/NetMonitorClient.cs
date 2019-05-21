7439a350849e9838e85a69fb2686fc3375f3f105
Илья Адолин <ilia-98@list.ru>
2019-05-21 15:33:18 +0500

    Сохранение

--- NetMonitorClient/NetMonitorClient.cs
+++ NetMonitorClient/NetMonitorClient.cs
@@ -13,6 +13,11 @@ using System.Net;
 using System.Drawing;
 using System.Threading;
 using System.Net.Mail;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Diagnostics;
+using System.Dynamic;
+using System.Runtime.InteropServices;
 
 namespace NetMonitorClient
 {
@@ -30,7 +35,7 @@ namespace NetMonitorClient
         static int CPULOADsensor_index;
         static List<int> HDDTEMPsensors_index = new List<int>();
 
-        public string Address { get; set; } = "127.0.0.1";
+        public string Address { get; set; } = "192.168.43.9";//"127.0.0.1";
         public static NotifyIcon NotifyIcon { get; set; }
         public static int Port { get; set; } = 1348;
         public static bool Enabled { get; set; } = true;
@@ -109,6 +114,142 @@ namespace NetMonitorClient
             }
         }
 
+
+        static HashSet<int> GetOpenPorts()
+        {
+            IPEndPoint[] ports = IPGlobalProperties.GetIPGlobalProperties().GetActiveTcpListeners();
+            HashSet<int> p_hashset = new HashSet<int>();
+            foreach (var item in ports)
+            {
+                p_hashset.Add(item.Port);
+            }
+            return p_hashset;
+        }
+
+        static HashSet<string> GetTcpConnections()
+        {
+            TcpConnectionInformation[] tcp_connections = IPGlobalProperties.GetIPGlobalProperties().GetActiveTcpConnections();
+            HashSet<string> tc_hashset = new HashSet<string>();
+            foreach (var item in tcp_connections)
+            {
+                tc_hashset.Add(item.RemoteEndPoint.ToString());
+            }
+            return tc_hashset;
+        }
+
+        static void GetAppInfo()
+        {
+            var disks = (from x in new ManagementObjectSearcher("SELECT * FROM Win32_Product").Get().OfType<ManagementObject>()
+                         select x);
+            string Q = "SELECT * FROM Win32_Product";
+            List<Dictionary<string, object>> result = new List<Dictionary<string, object>>();
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(Q);
+            foreach (ManagementObject obj in searcher.Get())
+            {
+                Dictionary<string, object> dict = new Dictionary<string, object>();
+                dict.Add("Name", obj.GetPropertyValue("Name"));
+                dict.Add("InstallDate", obj.GetPropertyValue("InstallDate"));
+                dict.Add("InstallLocation", obj.GetPropertyValue("InstallLocation"));
+                dict.Add("Vendor", obj.GetPropertyValue("Vendor"));
+                dict.Add("Version", obj.GetPropertyValue("Version"));
+                Console.WriteLine("\n");
+                foreach (var a in dict)
+                {
+                    Console.WriteLine(a.Key + " - " + a.Value);
+                }
+                result.Add(dict);
+            }
+            Console.Write(1);
+        }
+
+        struct ProcessInfo
+        {
+            string Name;
+            string CPUUsage;
+            string RAMUsage;
+            TimeSpan oldCPUTime;
+        }
+
+        struct Cpu
+        {
+            public double cpu;
+            public double ram;
+        }
+
+        static Cpu GetUsage(Process process)
+        {
+            // Getting information about current process
+            // Preparing variable for application instance name
+            string name = "";
+
+            foreach (var instance in new PerformanceCounterCategory("Process").GetInstanceNames())
+            {
+                if (instance.StartsWith(process.ProcessName))
+                {
+                    using (var processId = new PerformanceCounter("Process", "ID Process", instance, true))
+                    {
+                        if (process.Id == (int)processId.RawValue)
+                        {
+                            name = instance;
+                            break;
+                        }
+                    }
+                }
+            }
+            Cpu result = new Cpu();
+            try
+            {
+                var cpu = new PerformanceCounter("Process", "% Processor Time", name, true);
+                var ram = new PerformanceCounter("Process", "Private Bytes", name, true);
+
+                // Getting first initial values
+                cpu.NextValue();
+                ram.NextValue();
+
+                // Creating delay to get correct values of CPU usage during next query
+                Thread.Sleep(500);
+
+                // If system has multiple cores, that should be taken into account
+                result.cpu = Math.Round(cpu.NextValue() / Environment.ProcessorCount, 2);
+                // Returns number of MB consumed by application
+                result.ram = Math.Round(ram.NextValue() / 1024 / 1024, 2);
+            }
+            catch
+            {
+                result.cpu = 0;
+                result.ram = 0;
+            }
+            return result;
+
+        }
+
+        static void GetProc()
+        {
+            Process[] procList;
+            Dictionary<string, object> procdict = new Dictionary<string, object>();
+            while (true)
+            {
+                procList = Process.GetProcesses();
+                foreach (var item in procList)
+                {
+                    try
+                    {
+                        var cp = GetUsage(item).cpu;
+                        if (cp > 0)
+                        {
+                            Console.WriteLine();
+                            Console.WriteLine(cp);
+                            Console.WriteLine(item.MainModule.FileVersionInfo.FileDescription);
+                            Console.WriteLine(item.WorkingSet64 / 1024);
+                            Console.WriteLine(item.VirtualMemorySize64);
+                            Console.WriteLine(item.ProcessName);
+                        }
+                    }
+                    catch { }
+                }
+            }
+        }
+
         static Dictionary<string, string> GetHardwareInfo()
         {
             Dictionary<string, string> hardwareInfo = new Dictionary<string, string>();
@@ -177,6 +318,7 @@ namespace NetMonitorClient
 
         public void Start()
         {
+            //GetProc();
             socket = new WebSocket("ws://" + Address + ":" + Port + "/NetMonitorSocketService");
             UpdateSensors();
             socket.OnClose += Socket_OnClose;
@@ -193,14 +335,14 @@ namespace NetMonitorClient
             while (true)
             {
                 info = GetMonitorInfo();
-                if(info.CPU_temp > CriticalTemperature)
+                if (info.CPU_temp > CriticalTemperature)
                 {
                     try
                     {
                         smtpClient.Send(new MailMessage("netmonitor.client@gmail.com", EmailTo, "Warning", "Температура процессора превысила " + CriticalTemperature));
                     }
                     catch { }
-                    }
+                }
                 Thread.Sleep(10000);
             }
         }
@@ -227,20 +369,46 @@ namespace NetMonitorClient
         {
             try
             {
-                if (e.IsText)
+                Packet packet = e.RawData;
+                switch (packet.Header)
                 {
-                    if (e.Data == "Monitor_Info")
-                    {
-                        socket.Send(Packet.Serialize(new Packet() { Header = "Monitor_Info", Data = GetMonitorInfo() }));
-                    }
-
-                    if (e.Data == "Screenshot")
-                    {
-                        Bitmap printscreen = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
-                        Graphics graphics = Graphics.FromImage(printscreen);
-                        graphics.CopyFromScreen(0, 0, 0, 0, printscreen.Size);
-                        socket.Send(Packet.Serialize(new Packet() { Header = "Screenshot", Data = printscreen }));
-                    }
+                    case "Monitor_Info":
+                        {
+                            socket.Send(Packet.Serialize(new Packet() { Header = "Monitor_Info", Data = GetMonitorInfo() }));
+                        }
+                        break;
+                    case "Screenshot":
+                        {
+                            Bitmap printscreen = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+                            Graphics graphics = Graphics.FromImage(printscreen);
+                            graphics.CopyFromScreen(0, 0, 0, 0, printscreen.Size);
+                            socket.Send(Packet.Serialize(new Packet() { Header = "Screenshot", Data = printscreen }));
+                        }
+                        break;
+                    case "Files/GetUpdate":
+                        FileUtils.ОтправитьFilesUpdate(socket);
+                        break;
+                    case "Files/GetElementsFromPath":
+                        FileUtils.ОтправитьElementsFromPath(socket, packet);
+                        break;
+                    case "Files/RunFileOnPath":
+                        FileUtils.RunFileOnPath(socket, packet);
+                        break;
+                    case "Files/UploadFile":
+                        FileUtils.UploadFile(socket, packet);
+                        break;
+                    case "Files/GetFile":
+                        FileUtils.GetFile(socket, packet);
+                        break;
+                    case "Files/DeleteFile":
+                        FileUtils.DeleteFile(socket, packet);
+                        break;
+                    default:
+                        {
+                            socket.Send(new Packet() { Header = "Error", Data = packet });
+                            Console.WriteLine("Ошибка");
+                        }
+                        break;
                 }
             }
             catch { }
---
+++ NetMonitorClient/RemoteControl.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetMonitorClient
+{
+    class RemoteControl
+    {
+        [Flags]
+        public enum MouseEventFlags
+        {
+            LEFTDOWN = 0x00000002,
+            LEFTUP = 0x00000004,
+            MIDDLEDOWN = 0x00000020,
+            MIDDLEUP = 0x00000040,
+            MOVE = 0x00000001,
+            ABSOLUTE = 0x00008000,
+            RIGHTDOWN = 0x00000008,
+            RIGHTUP = 0x00000010
+        }
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint dwData, int dwExtraInfo);
+
+        void pressLeftMouse()
+        {
+            mouse_event((uint)MouseEventFlags.LEFTDOWN, 0, 0, 0, 0);
+            mouse_event((uint)MouseEventFlags.LEFTUP, 0, 0, 0, 0);
+        }
+
+    }
+}
